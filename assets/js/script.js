587eb6ec032f33711d711f86e2d69b6e3bd70e58
farrelleoin93 <farrelleoin93@gmail.com>
2021-01-07 15:13:39 +0000

    add show/hide functions to hanoi activities, culture and restaurants

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
-
+//------------------------------ Show cities information
 $(document).ready(function(){
   $("#hanoi-info").click(function(){
     $("#hanoi").show();
   });
+  
+//------------------------------ Show hanoi options information
+
+
+  $("#hanoi-activities").click(function(){
+    $("#hanoi-activities-info").show();
+    $("#hanoi-restaurants-info").hide();
+    $("#hanoi-culture-info").hide();
+  });
+
+   $("#hanoi-restaurants").click(function(){
+    $("#hanoi-restaurants-info").show();
+    $("#hanoi-activities-info").hide();
+    $("#hanoi-culture-info").hide();
+  });
+
+   $("#hanoi-culture").click(function(){
+    $("#hanoi-culture-info").show();
+    $("#hanoi-restaurants-info").hide();
+    $("#hanoi-activities-info").hide();
+  });
 }); 
+
+
+
