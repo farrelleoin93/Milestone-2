9117671071926077390702c55326e8bd8aba9a19
farrelleoin93 <farrelleoin93@gmail.com>
2021-01-11 22:29:16 +0000

    add cafe markers code to maps.js and update api key to include places library

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -9,6 +9,33 @@ function initMap() {
  let hanoi = new google.maps.LatLng(21.038598, 105.830440);
  let sapa = new google.maps.LatLng(22.336459, 103.843878);
  let hoian = new google.maps.LatLng(15.880314, 108.339319);
+ 
+var request = {
+    location: map.getCenter(),
+    radius: 8047,
+    types: ['cafe']
+  }
 
+  var service = new google.maps.places.PlacesService(map);
+
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    console.log(results.length);
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    title: place.name
+  })
 
 }
