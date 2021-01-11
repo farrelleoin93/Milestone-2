7c7b7bd5edf7ebabeb3dc83f6ee3b508697eab05
farrelleoin93 <farrelleoin93@gmail.com>
2021-01-11 21:50:16 +0000

    add link elements to hanoi choices items and add variables for sapa and hoian coords in maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,8 +1,14 @@
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map-hanoi"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 21.038598, lng: 105.830440 },
     zoom: 12,
   });
+
+ let hanoi = new google.maps.LatLng(21.038598, 105.830440);
+ let sapa = new google.maps.LatLng(22.336459, 103.843878);
+ let hoian = new google.maps.LatLng(15.880314, 108.339319);
+
+
 }
