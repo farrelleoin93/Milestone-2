aebb4e193bc64e9ae63a2a33f0ef9e160ad4983b
farrelleoin93 <farrelleoin93@gmail.com>
2021-01-12 18:34:55 +0000

    add maps for hanoi, sapa and hoi an

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,41 +1,17 @@
-let map;
+// Code for maps was found at https://thedebuggers.com/generate-multiple-google-maps/
 
+let map1, map2, map3;
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 21.038598, lng: 105.830440 },
-    zoom: 12,
-  });
 
- let hanoi = new google.maps.LatLng(21.038598, 105.830440);
- let sapa = new google.maps.LatLng(22.336459, 103.843878);
- let hoian = new google.maps.LatLng(15.880314, 108.339319);
- 
-var request = {
-    location: map.getCenter(),
-    radius: 8047,
-    types: ['cafe']
-  }
-
-  var service = new google.maps.places.PlacesService(map);
-
-  service.nearbySearch(request, callback);
-}
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    console.log(results.length);
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+    let mapOptions = {
+        zoom: 13,
     }
-  }
-}
+    mapOptions.center = new google.maps.LatLng(21.038598, 105.830440); // hanoi
+    map1 = new google.maps.Map(document.getElementById("map-hanoi"), mapOptions);
 
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location,
-    title: place.name
-  })
+    mapOptions.center = new google.maps.LatLng(22.336459, 103.843878); // sapa
+    map2 = new google.maps.Map(document.getElementById("map-sapa"), mapOptions);
 
+    mapOptions.center = new google.maps.LatLng(15.880314, 108.339319); // hoi-an
+    map2 = new google.maps.Map(document.getElementById("map-hoi-an"), mapOptions);
 }
