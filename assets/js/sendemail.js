b542f7440ff6917d5de2f46595d41f47f5bfb15d
farrelleoin93 <farrelleoin93@gmail.com>
2021-01-20 00:53:14 +0000

    add alert message to sendemail.js and tidy up code in index.html

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -7,8 +7,10 @@ function sendMail(contactForm) {
     .then(
         function() {
             let emailSent = document.getElementById("send-button");
-            emailSent.style.backgroundColor = "#e7a375";
+            emailSent.style.backgroundColor = "#00cc00";
+            emailSent.style.borderColor = "#00cc00";
             emailSent.innerHTML = "Sent!";
+            alert("Your email has been sent, a member of our team will be in touch shortly");
         },
         function(error) {
             alert("I'm sorry, something went wrong. Please make sure that all your details are correct and try again.", error);
