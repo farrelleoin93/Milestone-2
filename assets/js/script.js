009ab87f530c5232bd8edcb78cef19857103591e
farrelleoin93 <farrelleoin93@gmail.com>
2021-01-26 00:14:15 +0000

    add show and hide to city functions to reduce numbers of maps to 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,29 @@
 $(document).ready(function(){
   $("#hanoi-info").click(function(){
     $("#hanoi").show();
+    $("#hanoi-activities-info").show();
     $("#sapa").hide();
     $("#hoi-an").hide();
+    $("#sapa-activities-info").hide();
+    $("#hoi-an-activities-info").hide();
   });
 
   $("#sapa-info").click(function(){
     $("#sapa").show();
+    $("#sapa-activities-info").show();
     $("#hanoi").hide();
     $("#hoi-an").hide();
+    $("#hanoi-activities-info").hide();
+    $("#hoi-an-activities-info").hide();
   });
 
   $("#hoi-an-info").click(function(){
     $("#hoi-an").show();
+    $("#hoi-an-activities-info").show();
     $("#sapa").hide();
     $("#hanoi").hide();
+    $("#hanoi-activities-info").hide();
+    $("#sapa-activities-info").hide();
   });
 
   
