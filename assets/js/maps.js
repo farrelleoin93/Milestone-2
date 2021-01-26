43f6b0dab3be52800555c33b3fa0abf7f9550002
farrelleoin93 <farrelleoin93@gmail.com>
2021-01-26 00:17:09 +0000

    fix problems resulting from having multiple maps and add markers to new map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,52 +1,87 @@
 // Code for maps was found at https://thedebuggers.com/generate-multiple-google-maps/
-let map1, map2, map3;
+let map;
 let markers = [];
 
 
+// function getCity() {
+//     if(document.getElementById("hanoi").click) {
+//         return new google.maps.LatLng(21.038598, 105.830440); // hanoi
+//     } else if(document.getElementById("sapa").click) {
+//         return new google.maps.LatLng(22.336459, 103.843878); // sapa
+//     } else if(document.getElementById("hoi-an").click) {
+//         return google.maps.LatLng(15.880314, 108.339319); // hoi-an
+//     } 
+//     return null;
+// }
+
+
 function initMap() {
-    let mapOptions = {
+    map = new google.maps.Map(document.getElementById("map"), {
         zoom: 13,
-    }
-    mapOptions.center = new google.maps.LatLng(21.038598, 105.830440); // hanoi
-    map1 = new google.maps.Map(document.getElementById("map-hanoi"), mapOptions);
-    mapOptions.center = new google.maps.LatLng(22.336459, 103.843878); // sapa
-    map2 = new google.maps.Map(document.getElementById("map-sapa"), mapOptions);
-    mapOptions.center = new google.maps.LatLng(15.880314, 108.339319); // hoi-an
-    map2 = new google.maps.Map(document.getElementById("map-hoi-an"), mapOptions);
+        center: new google.maps.LatLng(21.038598, 105.830440),
+    })
 }
-function displayLocationsOfType(mapInstance, locationTypes) {
-     var request = {
-        location: mapInstance.getCenter(),
+
+// Code to change center of the map was found at https://stackoverflow.com/questions/28499141/how-to-change-google-map-center-by-clicking-a-button/28500306
+function newLocation(newLat, newLng) {
+    map.setCenter({
+        lat: newLat,
+        lng: newLng
+    });
+}
+
+//Setting Location with jQuery
+$(document).ready(function () {
+    $("#hanoi-info").click(function () {
+        newLocation(21.038598, 105.830440);
+    });
+
+    $("#sapa-info").click(function () {
+        newLocation(22.336459, 103.843878);
+    });
+
+    $("#hoi-an-info").click(function () {
+        newLocation(15.880314, 108.339319);
+    });
+});
+
+function displayLocationsOfType(locationTypes) {
+    var request = {
+        location: map.getCenter(),
         radius: 8047,
-        types: locationTypes
-      }
-    var service = new google.maps.places.PlacesService(mapInstance);
+        types: locationTypes,
+    }
+    var service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, callback);
 }
 function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    console.log(results.length);
-    for (var i = 0; i < results.length; i++) {
-      markers.push(results[i]);
-      createMarker(results[i]);
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results.length);
+        for (var i = 0; i < results.length; i++) {
+            markers.push(results[i]);
+            createMarker(results[i]);
+        }
     }
-  }
 }
 
 
 function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map1,
-    position: place.geometry.location,
-    title: place.name
-  })
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        title: place.name
+    })
 }
-
 
 function clearMarkers() {
-  callback(null);
+    callback(null);
 }
+
+
+// function clearMarkers() {
+//   callback(null);
+// }
 
 // function clearMarkers() {
 //   for (let i = 0; i < markers.length; i++) {
@@ -54,44 +89,55 @@ function clearMarkers() {
 //   }
 //   markers = [];
 // }
-
-
-function getMapInstanceFromPlaceNameIdentifier(placeNamesIdentifier) {
-    if(placeNamesIdentifier === 'hanoi') {
-        return map1;
-    } else if(placeNamesIdentifier === 'sapa') {
-        return map2;
-    } else if(placeNamesIdentifier === 'hoi-an') {
-        return map3;
-    } 
-    return null;
-}
-
-const placeNamesIdentifiers = ['hanoi', 'sapa', 'hoi-an'];
-placeNamesIdentifiers.forEach((eachPlaceIdentifier) => {
- $("#" + eachPlaceIdentifier + "-bars").click(function(){
-     clearMarkers();
-        const mapInstance = getMapInstanceFromPlaceNameIdentifier(eachPlaceIdentifier);
-        displayLocationsOfType(mapInstance, ['bar']);
-  });
-   $("#" + eachPlaceIdentifier + "-restaurants").click(function(){
-       clearMarkers();
-        const mapInstance = getMapInstanceFromPlaceNameIdentifier(eachPlaceIdentifier);
-        displayLocationsOfType(mapInstance, ['restaurant']);
-  });
-     $("#" + eachPlaceIdentifier + "-cafes").click(function(){
-         clearMarkers();
-        const mapInstance = getMapInstanceFromPlaceNameIdentifier(eachPlaceIdentifier);
-        displayLocationsOfType(mapInstance, ['cafe']);
-  });
-     $("#" + eachPlaceIdentifier + "-hotels").click(function(){
-         clearMarkers();
-        const mapInstance = getMapInstanceFromPlaceNameIdentifier(eachPlaceIdentifier);
-        displayLocationsOfType(mapInstance, ['lodging']);
-  });
-     $("#" + eachPlaceIdentifier + "-attractions").click(function(){
-         clearMarkers();
-        const mapInstance = getMapInstanceFromPlaceNameIdentifier(eachPlaceIdentifier);
-        displayLocationsOfType(mapInstance, ['tourist_attraction']);
-  });
+$(document).ready(function () {
+    $(".bars").click(function () {
+        clearMarkers();
+        displayLocationsOfType(["bar"]);
+    });
+    $(".restaurants").click(function () {
+        clearMarkers();
+        displayLocationsOfType(["restaurant"]);
+    });
+    $(".cafes").click(function () {
+        displayLocationsOfType(["cafe"]);
+    });
+    $(".hotels").click(function () {
+        clearMarkers();
+        displayLocationsOfType(["lodging"]);
+    });
+    $(".attractions").click(function () {
+        clearMarkers();
+        displayLocationsOfType(["tourist_attraction"]);
+    });
 });
+
+
+// const placeNamesIdentifiers = ['hanoi', 'sapa', 'hoi-an'];
+// placeNamesIdentifiers.forEach((eachPlaceIdentifier) => {
+//  $("#" + eachPlaceIdentifier + "-bars").click(function(){
+//     //  clearMarkers();
+//         const mapInstance = getMapInstanceFromPlaceNameIdentifier(eachPlaceIdentifier);
+//         displayLocationsOfType(['bar']);
+//   });
+//    $("#" + eachPlaceIdentifier + "-restaurants").click(function(){
+//     //    clearMarkers();
+//         const mapInstance = getMapInstanceFromPlaceNameIdentifier(eachPlaceIdentifier);
+//         displayLocationsOfType(['restaurant']);
+//   });
+//      $("#" + eachPlaceIdentifier + "-cafes").click(function(){
+//         //  clearMarkers();
+//         const mapInstance = getMapInstanceFromPlaceNameIdentifier(eachPlaceIdentifier);
+//         displayLocationsOfType(['cafe']);
+//   });
+//      $("#" + eachPlaceIdentifier + "-hotels").click(function(){
+//         //  clearMarkers();
+//         const mapInstance = getMapInstanceFromPlaceNameIdentifier(eachPlaceIdentifier);
+//         displayLocationsOfType(['lodging']);
+//   });
+//      $("#" + eachPlaceIdentifier + "-attractions").click(function(){
+//         //  clearMarkers();
+//         const mapInstance = getMapInstanceFromPlaceNameIdentifier(eachPlaceIdentifier);
+//         displayLocationsOfType(['tourist_attraction']);
+//   });
+// });
+
