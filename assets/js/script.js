9c3300a77c59165150aeffd263f294580a830912
farrelleoin93 <farrelleoin93@gmail.com>
2021-02-04 17:58:30 +0000

    change code so that it is easier to maintain and more condensed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,16 @@
 //------------------------------ Show cities information and display map
+const CITY_NAMES = ['hanoi', 'sapa', 'hoi-an'];
 $(document).ready(function(){
-  $("#hanoi-info").click(function(){
-    $("#hanoi").show();
-    $("#map-container").show();
-    $("#hanoi-activities-info").show();
-    $("#sapa").hide();
-    $("#hoi-an").hide();
-    $("#sapa-activities-info").hide();
-    $("#hoi-an-activities-info").hide();
-  });
-
-  $("#sapa-info").click(function(){
-    $("#sapa").show();
-    $("#map-container").show();
-    $("#sapa-activities-info").show();
-    $("#hanoi").hide();
-    $("#hoi-an").hide();
-    $("#hanoi-activities-info").hide();
-    $("#hoi-an-activities-info").hide();
-  });
-
-  $("#hoi-an-info").click(function(){
-    $("#hoi-an").show();
-    $("#map-container").show();
-    $("#hoi-an-activities-info").show();
-    $("#sapa").hide();
-    $("#hanoi").hide();
-    $("#hanoi-activities-info").hide();
-    $("#sapa-activities-info").hide();
+  CITY_NAMES.forEach(eachCityName => {
+     $(`#${eachCityName}-info`).click(function(){
+      $(`#${eachCityName}`).show();
+      $("#map-container").show();
+      $(`#${eachCityName}-activities-info`).show();
+       const otherCities = CITY_NAMES.filter(eachCity => eachCity !== eachCityName);
+       otherCities.forEach(otherCityItr => { 
+         $(`#${otherCityItr}`).hide();
+         $(`#${otherCityItr}-activities-info`).hide();
+       });
+    }); 
   });
 });
-
-
-
