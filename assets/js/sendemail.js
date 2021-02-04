d218c4dbedd7f69aa6d39327cfd05fb79fb316f8
farrelleoin93 <farrelleoin93@gmail.com>
2021-02-04 17:56:06 +0000

    change alert to show and hide paragraph for send email

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,23 +1,32 @@
 // Code to email the completed form was found at https://www.emailjs.com/docs/
 
 function sendMail(contactForm) {
-    emailjs.send("service_hbfvznd","discover_vietnam", {
+    emailjs.send("service_hbfvznd", "discover_vietnam", {
         "from_name": contactForm.fullname.value,
         "from_email": contactForm.emailaddress.value,
         "message": contactForm.message.value
     })
-    .then(
-        function() {
-            let emailSent = document.getElementById("send-button");
-            emailSent.style.backgroundColor = "#00cc00";
-            emailSent.style.borderColor = "#00cc00";
-            emailSent.innerHTML = "Sent!";
-            alert("Your email has been sent, a member of our team will be in touch shortly");
-        },
-        function(error) {
-            alert("I'm sorry, something went wrong. Please make sure that all your details are correct and try again.", error);
-        }
-    );
+        .then(
+            function () {
+                let emailSent = document.getElementById("success");
+                $("#success").show();
+                function toggleDiv() {
+                    setTimeout(function () {
+                        $("#success").hide();
+                    }, 5000);
+                }
+                toggleDiv();
+            },
+            function (error) {
+                $("#failed").show();
+                function toggleDiv() {
+                    setTimeout(function () {
+                        $("#failed").hide();
+                    }, 5000);
+                }
+                toggleDiv();
+            }
+        );
     document.getElementById("contactForm").reset();
     return false;
 }
